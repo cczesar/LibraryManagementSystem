b686708f6764634f3b9dcc1a95b06015b439e438
cczesar <cesarlaptop@laptopcesar.localdomain>
2026-04-06 17:56:37 -0700

    Added info team for drivers

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <Driver.h>
+#include <Team.h>
+
+"""date of data: 2026-04-06"""
+int main(){
+    Team mercedes("Mercedes", "Germany", 8, 134);
+    Team redbull("Redbull", "Austria", 6, 130);
+    team ferrari("Ferrari", "Italy", 16, 248);
+    team mclaren("McLaren", "England", 10, 203);
+}
+
+mercedes.addDriver("George Russel");
+mercedes.addDriver("Kimi Antonelli");
+
+redbull.addDriver("Max Verstappen");
+redbull.addDriver("Isack Hadjar");
+
+ferrari.addDriver("Charles Leclerc");
+ferrari.addDriver("Lewis Hamilton")
+
+mclaren.addDriver("Lando Norris");
+mclaren.addDriver("Oscar Piastri");
+
+Driver russel("George Russel", 63, "British", 0, 6, "Mercedes");
+Driver antonelli("Kimi Antonelli", 12, "Italian", 0, 2, "Mercedes");
+Driver verstappen("Max Verstappen", 3, "Dutch", 4, 71, "Redbull");
+Driver hadjar("Isack Hadjar", 6, "French", 0, 0, "Redbull");
+Driver leclerc("Charles Leclerc", 16, "Monegasque", 0, 8, "Ferrari");
+Driver hamilton("Lewis Hamilton", 44, "British", 7, 105, "Ferrari");
+Driver norris("Lando Norris", 1, "British", 1, 11, "McLaren");
+Driver piastri("Oscar Piastri", 81 "Australian", 0, 9, "McLaren");
+
