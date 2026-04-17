1243b67741f60dce38015ed0e1031bdadfcbf33b
cczesar <cesarlaptop@laptopcesar.localdomain>
2026-04-17 01:00:13 -0700

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
-#include <Driver.h>
-#include <Team.h>
+#include "Driver.h"
+#include "Library.h" 
+#include "Team.h"
 
-"""date of data: 2026-04-06"""
+//date of data: 2026-04-06
 int main(){
+
+    Library lib("data/drivers.csv", "data/teams.csv");
+
+
     Team mercedes("Mercedes", "Germany", 8, 134);
     Team redbull("Redbull", "Austria", 6, 130);
-    team ferrari("Ferrari", "Italy", 16, 248);
-    team mclaren("McLaren", "England", 10, 203);
+    Team ferrari("Ferrari", "Italy", 16, 248);
+    Team mclaren("McLaren", "England", 10, 203);
 
 
 mercedes.addDriver("George Russel");
@@ -17,74 +22,60 @@ redbull.addDriver("Max Verstappen");
 redbull.addDriver("Isack Hadjar");
 
 ferrari.addDriver("Charles Leclerc");
-ferrari.addDriver("Lewis Hamilton")
+ferrari.addDriver("Lewis Hamilton");
 
 mclaren.addDriver("Lando Norris");
 mclaren.addDriver("Oscar Piastri");
 
-Driver russel("George Russel", 63, "British", 0, 6, "Mercedes");
-Driver antonelli("Kimi Antonelli", 12, "Italian", 0, 2, "Mercedes");
-Driver verstappen("Max Verstappen", 3, "Dutch", 4, 71, "Redbull");
-Driver hadjar("Isack Hadjar", 6, "French", 0, 0, "Redbull");
-Driver leclerc("Charles Leclerc", 16, "Monegasque", 0, 8, "Ferrari");
-Driver hamilton("Lewis Hamilton", 44, "British", 7, 105, "Ferrari");
-Driver norris("Lando Norris", 1, "British", 1, 11, "McLaren");
-Driver piastri("Oscar Piastri", 81 "Australian", 0, 9, "McLaren");
+lib.addTeam(mercedes);
+lib.addTeam(redbull);
+lib.addTeam(ferrari);
+lib.addTeam(mclaren);
+
+lib.addDriver(Driver russel("George Russel", 63, "British", 0, 6, "Mercedes"));
+lib.addDriver(Driver antonelli("Kimi Antonelli", 12, "Italian", 0, 2, "Mercedes"));
+lib.addDriver(Driver verstappen("Max Verstappen", 3, "Dutch", 4, 71, "Redbull"));
+lib.addDriver(Driver hadjar("Isack Hadjar", 6, "French", 0, 0, "Redbull"));
+lib.addDriver(Driver leclerc("Charles Leclerc", 16, "Monegasque", 0, 8, "Ferrari"));
+lib.addDriver(Driver hamilton("Lewis Hamilton", 44, "British", 7, 105, "Ferrari"));
+lib.addDriver(Driver norris("Lando Norris", 1, "British", 1, 11, "McLaren"));
+lib.addDriver(Driver piastri("Oscar Piastri", 81, "Australian", 0, 9, "McLaren"));
+
+lib.displayAllTeams();
+lib.displayAllDrivers();
+
+std::cout << "\n===find and modify ===\n\n";
+Driver* d = lib.findDriver("Lando Norris");
+    if (D != nullptr){
+        std::cout << d* << "\n";
+    }
+
+Team* t = lib.findTeam("McLaren");
+if(t != nullptr){
+    std::cout << *t << "\n";
+
+}
+
+lib.displayDriversByTeam("Ferrari");
+lib.displayDriversByTeam("McLaren");
+lib.displayDriversByTeam("Mercedes");
+lib.displayDriversByTeam("Redbull");
 
 
-std::cout << "\n=== TEAMS ===\n\n";
-mercedes.display();
-redbull.display();
-ferrari.display();
-mclaren.display();
+std::cout << "\n=== sorted by wins ===\n\n";
+lib.sortDriversByWins();
+lib.displayAllDrivers();
 
-std::cout <<"\n=== DRIVERS ===\n\n";
-russel.display();
-antonelli.display();
-verstappen.display();
-hadjar.display();
-leclerc.display();
-hamilton.display();
-norris.display();
-piastri.display();
+lib.sortTeamsByWins();
+lib.displayAllTeams();
 
+try{
+    lib.saveToFiles();
+    std::cout << "\nData saved to CSV successfully \n";
+}
+catch (const std::runtime_error& e){
+    std::cout << "Error saving: "; << e.what() <<"\n";
+}
 
-std::cout << "\n=== QUICK LISTING ===\n\n";
-std::cout << russel << "\n";
-std::cout << antonelli << "\n";
-std::cout << verstappen << "\n";
-std::cout << hadjar << "\n";
-std::cout << leclerc << "\n";
-std::cout << hamilton << "\n";
-std::cout << norris << "\n";
-std::cout << piastri << "\n";
-
-std::cout << "\n=== CSV Output ===\n\n";
-std::cout << russel.toCSV() << "\n";
-std::cout << antonelli.toCSV() << "\n";
-std::cout << verstappen.toCSV() << "\n";
-std::cout << hadjar.toCSV() << "\n";
-std::cout << leclerc.toCSV() << "\n";
-std::cout << hamilton.toCSV() << "\n";
-std::cout << norris.toCSV() << "\n";
-std::cout << piastri.toCSV() << "\n";
-
-std::cout << "\n=== EQUALITY TEST ===\n\n";
-if (hamilton == hamilton)
-    std::cout << "same driver\n";
-if (!(hamilton == verstappen))
-    std::cout << "Different drivers\n";
-if (mercedes == mercedes)
-    std::cout << "Same team\n"
-if (!(mercedes == ferrari))
-    std::cout << "Different teams\n";
-
-
-std::cout << "\n=== HAS DRIVER TEST ===\n\n";
-    if (mercedes.hasDriver("George Russel"))
-        std::cout << "Hamilton is in mercedes\n";
-    if (!mercedes.hasDriver("Max Verstappen"))
-    std::cout << "Verstappen is NOT in mercedes AND AINT WINNING SHI\n";
-
-    return 0;
+return 0;
 }
